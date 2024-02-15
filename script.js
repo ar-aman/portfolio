74713440833c6a00ffabca026dbb5e1da2136e48
ar-aman <111566230+ar-aman@users.noreply.github.com>
2024-02-16 01:10:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,14 @@ document.addEventListener('scroll',() => {
           }
         }
       }
+
+      document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+    
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+    
