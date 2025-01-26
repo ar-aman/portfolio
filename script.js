8e72735fe434312c96275c74682dbe79fa5a3c65
ar-aman <you@example.comamanraj02722@gmail.com>
2025-01-26 07:26:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener('scroll',() => {
+  const header = document.querySelector('header');
+
+  if(window.scrollY > 0){
+      header.classList.add('scrolled');
+  }
+  else{
+      header.classList.remove('scrolled');
+  }
+  });
+
+  function myFunction() {
+      document.getElementById("myDropdown").classList.toggle("show");
+    }
+    
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function(event) {
+      if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+      }
+    }
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+  
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+              behavior: 'smooth'
+          });
+      });
+  });
+  
